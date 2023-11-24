2da87eedbe72bb7c28ac343947fc49edb41d62b8
olikol <78661364+olikols07@users.noreply.github.com>
2023-11-24 14:13:16 +0100

    Added Construction page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/6f1611ac97.js" crossorigin="anonymous"></script>
+</svelte:head>
+<slot></slot>
+<style>
+    :root {
+        font-family: 'Comfortaa', sans-serif;
+    }
+</style>
